fb5513dcb5099cb4a39f5b899eb5f04f879f205b
Ryan Ghavimi <rghavimi@uci.edu>
2022-03-26 01:02:31 -0700

    Added user dashboard connected to openai

--- resources/views/themes/tailwind/dashboard/index.blade.php
+++ resources/views/themes/tailwind/dashboard/index.blade.php
@@ -4,56 +4,95 @@
 @section('content')
 
 
-	<div class="flex flex-col px-8 mx-auto my-6 lg:flex-row max-w-7xl xl:px-5">
-	    <div class="flex flex-col justify-start flex-1 mb-5 overflow-hidden bg-white border rounded-lg lg:mr-3 lg:mb-0 border-gray-150">
-	        <div class="flex flex-wrap items-center justify-between p-5 bg-white border-b border-gray-150 sm:flex-no-wrap">
-				<div class="flex items-center justify-center w-12 h-12 mr-5 rounded-lg bg-wave-100">
-					<svg class="w-6 h-6 text-wave-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+<!-- Section 1 -->
+<section class="w-full bg-white">
+<header class="bg-white shadow">
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+      <h1 class="text-3xl font-bold text-gray-900">Dashboard</h1>
+    </div>
+  </header>
+<div class="mx-auto max-w-7xl">
+	<div class="flex flex-col lg:flex-row">
+		<div class="relative w-full bg-cover lg:w-6/12 xl:w-7/12 bg-gradient-to-r from-white via-white to-gray-100">
+			<div class="relative flex flex-col items-center justify-center w-full h-full px-10 my-20 lg:px-16 lg:my-0">
+				<div class="flex flex-col items-start space-y-8 tracking-tight lg:max-w-3xl">
+					<div class="relative">
+						<p class="mb-2 font-medium text-gray-700 uppercase">Work smarter</p>
+						<h2 class="text-5xl font-bold text-gray-900 xl:text-6xl">Create your ad copy in seconds</h2>
+					</div>
+					<p class="text-2xl text-gray-700">We've created a simple formula to follow in order to gain more out of your business.</p>
+					<a href="#_" class="inline-block px-8 py-5 text-xl font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease">Get Started Today</a>
 				</div>
-				<div class="relative flex-1">
-	                <h3 class="text-lg font-medium leading-6 text-gray-700">
-	                    Welcome to your Dashboard
-	                </h3>
-	                <p class="text-sm leading-5 text-gray-500 mt">
-	                    Learn More Below
-	                </p>
-				</div>
-
-	        </div>
-	        <div class="relative p-5">
-	            <p class="text-base leading-loose text-gray-500">This is your application <a href="{{ route('wave.dashboard') }}" class="underline text-wave-500">dashboard</a>, you can customize this view inside of <code class="px-2 py-1 font-mono text-base font-medium text-gray-600 bg-gray-100 rounded-md">{{ theme_folder('/dashboard/index.blade.php') }}</code><br><br> (Themes are located inside the <code>resources/views/themes</code> folder)</p>
-				<span class="inline-flex mt-5 rounded-md shadow-sm">
-	                <a href="{{ url('docs') }}" class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50">
-	                    Read The Docs
-	                </a>
-				</span>
 			</div>
 		</div>
-		<div class="flex flex-col justify-start flex-1 overflow-hidden bg-white border rounded-lg lg:ml-3 border-gray-150">
-	        <div class="flex flex-wrap items-center justify-between p-5 bg-white border-b border-gray-150 sm:flex-no-wrap">
-				<div class="flex items-center justify-center w-12 h-12 mr-5 rounded-lg bg-wave-100">
-					<svg class="w-6 h-6 text-wave-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z"></path></svg>
-				</div>
-				<div class="relative flex-1">
-	                <h3 class="text-lg font-medium leading-6 text-gray-700">
-						Learn more about Wave
-	                </h3>
-	                <p class="text-sm leading-5 text-gray-500 mt">
-						Are you more of a visual learner?
-	                </p>
-				</div>
-
-	        </div>
-	        <div class="relative p-5">
-				<p class="text-base leading-loose text-gray-500">Make sure to head on over to the Wave Video Tutorials to learn more how to use and customize Wave.<br><br>Click on the button below to checkout the video tutorials.</p>
-				<span class="inline-flex mt-5 rounded-md shadow-sm">
-	                <a href="https://devdojo.com/course/wave" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50">
-						Watch The Videos
-	                </a>
-				</span>
-			</div>
-	    </div>
-
 	</div>
+</div>
+    <div class="mx-auto max-w-7xl">
+        <div class="flex flex-col lg:flex-row">
+            <div class="relative w-full bg-cover lg:w-6/12 xl:w-7/12 bg-gradient-to-r from-white via-white to-gray-100">
+                <div class="relative flex flex-col items-center justify-center w-full h-full px-10 my-20 lg:px-16 lg:my-0">
+                    <div class="flex flex-col items-start space-y-8 tracking-tight lg:max-w-3xl">
+					<div class="relative">
+                            <label class="font-medium text-gray-900">Name</label>
+                            <input type="text" class="block w-full px-50 py-50 mt-2 text-xl placeholder-gray-100 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Enter Your Product Description">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-full bg-white lg:w-6/12 xl:w-5/12">
+                <div class="flex flex-col items-start justify-start w-full h-full p-10 lg:p-16 xl:p-24">
+                        <div class="relative">
+                            <label class="font-medium text-gray-900">Ad Copy</label>
+                            <input type="text" class="block w-full px-50 py-50 mt-2 text-xl placeholder-gray-100 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50">
+                        </div>
+                </div>
+            </div>
+			
+        </div>
+		<div class="relative">
+            <a onClick="createAdCopy()" class="inline-block w-full px-5 py-4 mt-3 text-lg font-bold text-center text-gray-900 transition duration-200 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 ease">Submit</a>
+        </div>
+    </div>
+
+
+    <script>
+
+async function createAdCopy(){
+    let url = "https://api.openai.com/v1/engines/text-davinci-002/completions";
+    let requestBody = JSON.stringify({
+                                "prompt": "Write a creative ad for the following product to run on Facebook aimed at parents:\n\nProduct: Learning Room is a virtual environment to help students from kindergarten to high school excel in school.", 
+                                "temperature": 0.5,
+                                "max_tokens": 60,
+                                "top_p": 1.0,
+                                "frequency_penalty": 0.0,
+                                "presence_penalty": 0.0
+                            });
+    console.log(requestBody);
+	var rawResponse = await fetch(url, {
+                            method: 'POST',
+                            headers: {
+                            "Authorization": 'Basic '+ btoa(':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'),
+                            "Content-type": "application/json"
+                            },
+                            body: requestBody
+                        });
+                        // .then(json)
+                        // .then(function (data) {
+                        //     console.log('Request succeeded with JSON response', data);
+                        // })
+                        // .catch(function (error) {
+                        //     console.log('Request failed', error);
+                        // });
+    const content = await rawResponse.json();
+
+	  console.log(content);
+}
+
+</script>
+
+</section>
+
+
 
 @endsection
